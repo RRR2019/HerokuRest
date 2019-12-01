051ccd055dc53842d327cf8a289c8924ab7c19b1
Rodrigo Robles <rrobles.rodrigo@gmail.com>
2019-12-01 15:11:39 -0500

    heroku

--- client/src/components/withAuth.js
+++ client/src/components/withAuth.js
@@ -5,7 +5,7 @@ import AuthService from './AuthService';
 
 export default function withAuth(AuthComponent) {
     // const Auth = new AuthService('http://localhost:3001');
-        const Auth = new AuthService('https://obscure-shore-32335.herokuapp.com');
+    const Auth = new AuthService('https://obscure-shore-32335.herokuapp.com');
 
     
        return class AuthWrapped extends Component {
