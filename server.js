c939a4e52074196633f92decb66b2cfd6dcea74d
Rodrigo Robles <rrobles.rodrigo@gmail.com>
2019-12-03 18:47:17 -0500

    heroku

--- server.js
+++ server.js
@@ -28,7 +28,8 @@ app.use(express.static("public"));
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static("client/build"));
+  // app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
